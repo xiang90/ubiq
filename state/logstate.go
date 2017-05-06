893c750c09fb5795a283df7dd42a333dae2f788b
Xiang <xiangli.cs@gmail.com>
2017-05-05 19:37:42 -0700

    state: update server impl

--- state/logstate.go
+++ state/logstate.go
@@ -9,19 +9,40 @@ var (
 )
 
 type logState struct {
-	fileName string
-	beg      int64
-	end      int64
-	state    logStateType
+	beg   int64
+	end   int64
+	state logStateType
 }
 
-func (a *logState) merge(b *logState) bool {
-	if a.end != b.beg {
+type logStates struct {
+	fileName string
+
+	states []*logState
+}
+
+func newLogStates(name string, end int64) *logStates {
+	return &logStates{
+		fileName: name,
+		states:   []*logState{{end: end, state: logStateWaiting}},
+	}
+}
+
+func (ls *logStates) merge(e Event) bool {
+	last := ls.last()
+
+	if last.end > e.Size {
+		// duplicate notification
 		return false
 	}
-	if a.state == b.state {
-		a.end = b.end
-		return true
+
+	if last.state == logStateWaiting {
+		last.end = e.Size
+	} else {
+		ls.states = append(ls.states, &logState{beg: last.end + 1, end: e.Size, state: logStateWaiting})
 	}
-	return false
+	return true
+}
+
+func (ls *logStates) last() *logState {
+	return ls.states[len(ls.states)-1]
 }
--- state/server.go
+++ state/server.go
@@ -7,36 +7,26 @@ var (
 )
 
 type Server struct {
-	logs map[string]logState
+	logs map[string]*logStates
 }
 
+// Notify notifies the state server with an event from the log data tracker.
 func (s *Server) Notify(e Event) error {
 	switch e.Type {
 	case EventNew:
 		if _, ok := s.logs[e.FileName]; ok {
 			return nil
 		}
-		s.logs[e.FileName] = logState{
-			fileName: e.FileName,
-			beg:      0,
-			end:      e.Size,
-		}
+		s.logs[e.FileName] = newLogStates(e.FileName, e.Size)
 		// todo: persist to etcd
 	case EventIncr:
 		ls, ok := s.logs[e.FileName]
 		if !ok {
 			return ErrNotFound
 		}
-		if ls.end > e.Size {
-			// duplicate notification
-			return nil
+		if ok := ls.merge(e); ok {
+			// todo: persist to etcd
 		}
-		nls := &logState{
-			beg:   ls.end,
-			end:   e.Size,
-			state: logStateWaiting,
-		}
-		ls.merge(nls)
 	}
 
 	return nil
