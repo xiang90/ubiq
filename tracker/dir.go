b8d4016eb0b03e60f75e19a08900ba9c4425203d
Cong Ding <dinggnu@gmail.com>
2017-05-06 23:40:18 -0400

    tracker: implement tracker with some TODOs

--- tracker/dir.go
+++ tracker/dir.go
@@ -1,5 +1,11 @@
 package tracker
 
+import (
+	"io/ioutil"
+	"path/filepath"
+	"time"
+)
+
 const (
 	trackSuffix = ".ubiq"
 )
@@ -16,8 +22,10 @@ var (
 )
 
 type event struct {
-	t      eventType
-	offset int64
+	t          eventType
+	origOffset int64
+	offset     int64
+	fileName   string
 }
 
 type dirTracker struct {
@@ -26,6 +34,48 @@ type dirTracker struct {
 }
 
 func (dt *dirTracker) track() (<-chan event, error) {
+	// check if the directory exsits
+	_, err := ioutil.ReadDir(dt.dir)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	eventC := make(chan event) // TODO size
+
+	go func() {
+		for {
+			files, err := ioutil.ReadDir(dt.dir)
+			if err != nil {
+				// TODO log the error
+				continue
+			}
+			for _, file := range files {
+				fn := file.Name()
+				// only track files with proper trackSuffix
+				if filepath.Ext(fn) != trackSuffix {
+					continue
+				}
+				if ft, ok := dt.files[fn]; ok {
+					// generate a fileIncr event for exsiting files
+					n := file.Size()
+					if n > ft.offset {
+						e := event{t: fileIncr, origOffset: ft.offset, offset: n, fileName: fn}
+						ft.offset = n
+						dt.files[fn] = ft
+						eventC <- e
+					}
+				} else {
+					// generate a fileNew event for new files
+					n := file.Size()
+					dt.files[fn] = fileTracker{offset: n}
+					e := event{t: fileNew, origOffset: 0, offset: n, fileName: fn}
+					eventC <- e
+				}
+			}
+			// TODO make the size configurable
+			time.Sleep(10 * time.Millisecond)
+		}
+	}()
+
+	return eventC, nil
 }
--- tracker/tracker.go
+++ tracker/tracker.go
@@ -5,11 +5,22 @@ type DirServer struct {
 }
 
 func NewDirServer(root string) (*DirServer, error) {
-	return nil, nil
+	s := &DirServer{dirs: make([]dirTracker, 0)}
+	return s, nil
 }
 
 func (s *DirServer) Start() error {
 	return nil
+}
+
+func (s *DirServer) AddDir(dir string) (<-chan event, error) {
+	dt := dirTracker{dir: dir}
+	ch, err := dt.track()
+	if err != nil {
+		return nil, err
+	}
+	s.dirs = append(s.dirs, dt)
+	return ch, nil
 }
 
 type S3Server struct {
